1daf964d6c7d7e215650f39a51ef69b84ff809b6
hui.yang <15868172375@163.com>
2020-12-01 18:42:58 +0800

    lamdada test demo

--- SpringEasy/src/test/java/lamada/LamadaTest.java
+++ SpringEasy/src/test/java/lamada/LamadaTest.java
@@ -1,10 +1,12 @@
 package lamada;
 
+import com.alibaba.fastjson.JSON;
 import com.yanghui.jackson.Student;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -41,6 +43,33 @@ public class LamadaTest {
         studentsList.add(new Student("test4", 10, null));
         System.out.println(studentsList.stream().map(Student::getAge).collect(Collectors.toList()));
         System.out.println(studentsList.stream().map(Student::getName).collect(Collectors.toList()));
+    }
+
+    //collectors.groupBy
+    @Test
+    public void test3() {
+        List<Student> studentsList = new ArrayList<>();
+        studentsList.add(new Student("test1", 20, null));
+        studentsList.add(new Student("test2", 33, null));
+        studentsList.add(new Student("test3", 22, null));
+        studentsList.add(new Student("test3", 88, null));
+        studentsList.add(new Student("test1", 222, null));
+        studentsList.add(new Student("test4", 10, null));
+        Map<String, List<Student>> map = studentsList.stream().collect(
+                Collectors.groupingBy(Student::getName)
+        );
+        //filter
+        Map<String, List<Student>> map2 = studentsList.stream().filter(
+                student -> student.getAge() == 33
+        ).collect(
+                Collectors.groupingBy(Student::getName)
+        );
+
+        List<Student> students = studentsList.stream().filter(student ->
+                student.getAge() < 30
+        ).collect(Collectors.toList());
+
+        System.out.println(JSON.toJSONString(map));
     }
 
 
