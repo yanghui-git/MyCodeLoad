a983f24f3ebe158663bfeec99543b2a431c4b5a6
hui.yang <15868172375@163.com>
2020-12-02 14:40:19 +0800

    java 并发包concurrent  CountDownLatch和CyclicBarrier的理解和区别

--- SpringEasy/src/main/java/com/yanghui/MuchThread/CountDownLatchCompare.java
+++ SpringEasy/src/main/java/com/yanghui/MuchThread/CountDownLatchCompare.java
@@ -1,18 +1,23 @@
 package com.yanghui.MuchThread;
 
-import com.yanghui.jackson.Student;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * https://liangyihuai.blog.csdn.net/article/details/83106584?utm_medium=distribute.pc_relevant.none-task-blog-OPENSEARCH-1
+ * .control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-OPENSEARCH-1.control
+ * Java的CountDownLatch和CyclicBarrier的理解和区别
+ */
 public class CountDownLatchCompare {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CountDownLatch countDownLatch = new CountDownLatch(5);
-        //  for(int i=0;i<countDownLatch.getCount();i)
+        for (int i = 0; i < countDownLatch.getCount(); i++) {
+            new Thread(new MyThread(countDownLatch), "玩家" + i).start();
+        }
+        System.out.println("正在等待所有玩家准备好");
+        countDownLatch.await();
+        System.out.println("开始游戏 go go go");
     }
 
     private static class MyThread implements Runnable {
