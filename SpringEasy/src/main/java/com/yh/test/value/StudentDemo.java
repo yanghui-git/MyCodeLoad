554f191bb477bc54f50c398bb14dfe8075c7087c
hui.yang <15868172375@163.com>
2020-12-30 09:37:13 +0800

    @VALUE 设置默认值demo

---
+++ SpringEasy/src/main/java/com/yh/test/value/StudentDemo.java
@@ -0,0 +1,11 @@
+package com.yh.test.value;
+
+import lombok.Data;
+
+@Data
+public class StudentDemo {
+
+    private String name;
+
+    private int age;
+}
---
+++ SpringEasy/src/main/java/com/yh/test/value/ValueDemo.java
@@ -0,0 +1,30 @@
+package com.yh.test.value;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class ValueDemo {
+
+    /**
+     * https://blog.csdn.net/zhangzehai2234/article/details/88564759
+     * @Value 设备默认值
+     */
+    @Value("${test.name2:HaHa}")
+    private String name;
+
+    @Value("${test.age}")
+    private int age;
+
+
+    @Bean
+    public StudentDemo getStudentDemo() {
+        StudentDemo studentDemo = new StudentDemo();
+        studentDemo.setAge(age);
+        studentDemo.setName(name);
+        System.out.println("name--   " + name + "   age--  " + age);
+        return studentDemo;
+    }
+}
