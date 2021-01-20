b8252279d1820d42a4fc02ba5451bd8ad392a499
hui.yang <15868172375@163.com>
2021-01-20 10:41:24 +0800

    Java LocalDateTime

---
+++ SpringEasy/src/main/java/com/yh/date/LocalDateTest.java
@@ -0,0 +1,17 @@
+package com.yh.date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * 日期 LocalDateTime 时间日期
+ * https://www.jianshu.com/p/048ee8580639
+ */
+public class LocalDateTest {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
