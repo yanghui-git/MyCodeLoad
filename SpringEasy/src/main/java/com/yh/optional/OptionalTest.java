aa5ceb846b383f44c3cdd61cef3397a1d9b38d5a
hui.yang <15868172375@163.com>
2021-02-24 10:28:55 +0800

    优雅代码 Optional

--- SpringEasy/src/main/java/com/yh/optional/OptionalTest.java
+++ SpringEasy/src/main/java/com/yh/optional/OptionalTest.java
@@ -3,8 +3,10 @@ package com.yh.optional;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.yaml.snakeyaml.representer.Represent;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -29,6 +31,29 @@ public class OptionalTest {
         studentOption = Optional.ofNullable(studentOption).orElse(new StudentOption());
         test = Optional.ofNullable(studentOption.getName()).orElse("3");
         System.out.println(test);
+
+        String tes = "9999";
+        Optional.ofNullable(tes).ifPresent(
+                new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println("xixixiix");
+                    }
+                }
+        );
+
+
+        Optional.ofNullable(tes).ifPresent(
+                s -> System.out.println(s)
+        );
+
+        Optional.ofNullable(tes).ifPresent(
+                s -> {
+                    System.out.println(s);
+                }
+        );
+
+
     }
 
 
