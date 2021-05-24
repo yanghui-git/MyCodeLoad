08850154196bf52160dcd394a0083c7216d9be9b
hui.yang <15868172375@163.com>
2021-05-24 16:48:23 +0800

    spring @ConditionalOnProperty

---
+++ SpringEasy/src/main/java/com/yh/test/dependson/ConditionalOnPropertTest.java
@@ -0,0 +1,11 @@
+package com.yh.test.dependson;
+
+import lombok.Data;
+
+@Data
+public class ConditionalOnPropertTest {
+
+    private String name;
+
+    private String age;
+}
--- SpringEasy/src/main/java/com/yh/test/dependson/DependsOnTest.java
+++ SpringEasy/src/main/java/com/yh/test/dependson/DependsOnTest.java
@@ -1,5 +1,6 @@
 package com.yh.test.dependson;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,13 @@ public class DependsOnTest {
         Thread.sleep(3000l);
         System.out.println("DependTestTwo");
         return new DependTestTwo();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "ConditionalOnProperty",havingValue = "123")
+    public ConditionalOnPropertTest getConditionalOnPropert(){
+        System.out.println("6666666666");
+        return null;
     }
 
 
